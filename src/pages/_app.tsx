26d905f5faa1345222883f085f46ca4859d87da8
Moisés <moisesnetored@gmail.com>
2022-09-07 18:22:27 -0400

    feat: header, Home page and Product component created
    Using Stitches I created the Header and the Home page, the images are currently being used the
    future image of the next, stay tuned when the next has any updates.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
 import { AppProps } from 'next/app';
+import Image from 'next/future/image';
 import { globalStyles } from '../styles/global';
+
+import logoImg from '../assets/logo.svg';
+import { Container, Header } from '../styles/pages/app';
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="Imagem da logo" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  );
 }
