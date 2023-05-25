fabc1922967451a641478e8fcf0640febd81a67b
Moisés <moisesnetored@gmail.com>
2023-05-25 15:10:06 -0400

    feat: creating application routes

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Ignite Shop</h1>
+    </div>
+  )
+}
---
+++ src/pages/product/[id].tsx
@@ -0,0 +1,9 @@
+import { useRouter } from "next/router"
+
+export default function Product(){
+  const {query} = useRouter()
+
+  return(
+    <div><h1>Product: {JSON.stringify(query)}</h1></div>
+  )
+}
