25b577ed02da0811ee8b168e043c7d31fcc1187b
Moisés <moisesnetored@gmail.com>
2023-05-25 15:50:40 -0400

    feat: application header

---
+++ src/pages/_app.page.tsx
@@ -0,0 +1,19 @@
+import { globalStyles } from '@/styles/global'
+import type { AppProps } from 'next/app'
+
+import logoImg from "@/assets/logo.svg"
+import { Container, Header } from './styles'
+
+globalStyles()
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
+}
