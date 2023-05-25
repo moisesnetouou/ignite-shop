c7787eb40c470129ea34ba3dac889844337133bb
Moisés <moisesnetored@gmail.com>
2023-05-25 15:59:48 -0400

    feat: images in next.js

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,8 +1,10 @@
 import { globalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
 
-import logoImg from "@/assets/logo.svg"
+// import logoImg from "@/assets/logo.svg"
+import homeImg from "@/assets/home.png"
 import { Container, Header } from './styles'
+import Image from 'next/image'
 
 globalStyles()
 
@@ -10,7 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} alt="" />
+        <Image src={homeImg} alt="" width={400} />
       </Header>
 
       <Component {...pageProps} />
