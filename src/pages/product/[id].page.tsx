9a98a3d3e6f7aedbb401bba1107a4b917167894b
Moisés <moisesnetored@gmail.com>
2023-05-26 11:53:30 -0400

    feat: product page structure

--- src/pages/product/[id].page.tsx
+++ src/pages/product/[id].page.tsx
@@ -1,9 +1,25 @@
 import { useRouter } from "next/router"
+import { ImageContainer, ProductContainer, ProductDetails } from "./styles"
 
 export default function Product(){
   const {query} = useRouter()
 
   return(
-    <div><h1>Product: {JSON.stringify(query)}</h1></div>
+    <ProductContainer>
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem obcaecati beatae voluptatibus temporibus doloribus iure dicta quod numquam culpa, eligendi labore corporis inventore eius magni expedita doloremque, quibusdam voluptatem. Temporibus!</p>
+
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
